20fddb374ee5c6f85636a55dbb38db67a43aeeca
angrykoala <angrykoala@outlook.es>
2021-04-06 00:51:31 +0200

    Minor code fix

--- entities/buildings/scenes/house/house.gd
+++ entities/buildings/scenes/house/house.gd
@@ -4,8 +4,9 @@ export(Global.RESOURCES) var required_resource
 export var consumption_rate:float=1
 export var max_required_storage:int=5
 export var max_market_distance:int= 10
+export var max_food_storage:int=10
 
-var current_required_quantity: float = 0
+var current_food_ammount: float=0
 
 
 func _ready():
@@ -18,16 +19,16 @@ func _exit_tree():
 func on_building_update(delta: float): # TODO: improve
 	.on_building_update(delta)
 	_consume_resource(delta)
-	if (current_required_quantity<consumption_rate or current_required_quantity==0):
+	if (current_food_ammount<consumption_rate or current_food_ammount==0):
 		_find_market_food()
 
 func _consume_resource(delta: float):
 	if _has_required_quantity(delta):
 		var consumed_ammount=consumption_rate*delta
-		current_required_quantity=clamp(current_required_quantity-consumed_ammount, 0, max_required_storage)
+		current_food_ammount=clamp(current_food_ammount-consumed_ammount, 0, max_required_storage)
 
 func _has_required_quantity(delta: float):
-	return current_required_quantity >= consumption_rate*delta
+	return current_food_ammount >= consumption_rate*delta
 
 
 func _find_market_food():
