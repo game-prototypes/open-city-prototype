44280521d97fe2c4626d43d4dc78474041882a5c
angrykoala <angrykoala@outlook.es>
2021-04-27 00:13:49 +0200

    Update tests

--- tests/city_test.gd
+++ tests/city_test.gd
@@ -5,17 +5,17 @@ func before_each():
 
 
 func test_add_money():
-	var original_money=City.money
+	var original_money=City.get_money()
 	var new_money=original_money+10
 	
 	City.add_money(10)
-	assert_eq(City.money, new_money)
+	assert_eq(City.get_money(), new_money)
 	assert_signal_emitted_with_parameters(City, "money_updated", [new_money])
 
 func test_remove_money():
-	var original_money=City.money
+	var original_money=City.get_money()
 	var new_money=original_money-10
 	
 	City.remove_money(10)
-	assert_eq(City.money, new_money)
+	assert_eq(City.get_money(), new_money)
 	assert_signal_emitted_with_parameters(City, "money_updated", [new_money])
