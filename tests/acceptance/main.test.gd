4becb61dc8e0e318263b08b30c9ebea759c02516
angrykoala <angrykoala@outlook.es>
2021-05-06 20:58:40 +0200

    fix tests

--- tests/acceptance/main.test.gd
+++ tests/acceptance/main.test.gd
@@ -7,7 +7,7 @@ func test_player_building():
 	simulate(main, 1, 10)
 	assert_eq(main.map._elements.get_children().size(), 0)
 	
-	main.gui.emit_signal("building_resource_selected", main.buildings[0])
+	main.hud.emit_signal("building_resource_selected", main.buildings[0])
 	main.map.emit_signal("tile_selected", Vector2(10,10), null)
 	simulate(main, 1, 100)
 	
